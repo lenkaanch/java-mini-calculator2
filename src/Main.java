6a2c096e602ff79e18875f7a229779ca779b8540
Elena Anch <107437613+lenkaanch@users.noreply.github.com>
2026-02-05 21:19:32 +0300

    improve output messages

--- src/Main.java
+++ src/Main.java
@@ -4,14 +4,16 @@ public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        System.out.println("=== Simple Java Calculator ===");
+
         System.out.print("Enter first number: ");
         int a = scanner.nextInt();
 
         System.out.print("Enter second number: ");
         int b = scanner.nextInt();
 
-        int sum = a + b;
-        System.out.println("Sum: " + sum);
+        System.out.println("Result:");
+        System.out.println(a + " + " + b + " = " + (a + b));
 
         scanner.close();
     }
